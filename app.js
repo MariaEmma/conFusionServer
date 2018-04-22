abc8e4a063b3bcb1cff1ae4e19a1809d9362e428
MariaEmma <emmanouil.maria@gmail.com>
2018-04-22 21:53:11 +0300

    Secure https and facebook passport login

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var config = require('./config');
 
@@ -31,6 +32,15 @@ mongoose.connect(url).then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -53,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use('/imageUpload',uploadRouter);
 
 app.use('/', index);
 app.use('/users', users);
