3544dded946b794e2ad2e3948a024911ed2898b9
MariaEmma <emmanouil.maria@gmail.com>
2018-05-13 00:38:38 +0300

    Add user favorite dishes endpoints

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 const uploadRouter = require('./routes/uploadRouter');
 
 var config = require('./config');
@@ -87,6 +88,7 @@ app.use(auth);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
